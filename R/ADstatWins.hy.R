9e1f51c9cdf7d40ee062d383f05c7f3b6cfe08b5
Hyo Young Choi <lucienne1216@gmail.com>
2018-03-14 23:51:34 -0400

    manual edited

--- R/ADstatWins.hy.R
+++ R/ADstatWins.hy.R
@@ -1,8 +1,16 @@
+#' Provide the winsorized Anderson-Darling test statistic
+#'
+#' This function winsorizes and calculates the A-D test statistic.
+#' Based on the MATLAB function ADStatQF by Qing Feng.
+#'
+#' @param x an input vector
+#'
+#' @export
 ADstatWins.hy=function(x){
   # Winsorize and calculate Anderson Darling test statistic
   n = length(x);
   mabd = mean(abs(x-median(x)));
-  
+
   if (mabd==0){
     xfinal = rep(0,n);
   } else {
@@ -14,11 +22,11 @@ ADstatWins.hy=function(x){
     L = p95*an+bn;
     xk[which(xk>L)] = rep(L,length(which(xk>L)));
     xk[which(xk<(-L))] = rep((-L),length(which(xk<(-L))));
-    
+
     # Re-standardize
     xfinal = (xk-mean(xk))/sd(xk);
   }
-  
+
   # Calculate evaluation stat
   ADval = ADstat.hy(xfinal);
   return(ADval);
