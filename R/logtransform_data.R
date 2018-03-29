744f01d834dcdc3c4a39baba8cda042b4cafbbc8
Hyo Young Choi <lucienne1216@gmail.com>
2018-03-28 20:09:43 -0400

    param.grid added

--- R/logtransform_data.R
+++ R/logtransform_data.R
@@ -7,11 +7,11 @@
 #' @export
 #' @examples
 
-logtransform_data = function(data,logshift.val=NULL, ...) {
+logtransform_data = function(data,logshift.val=NULL, param.grid=NULL, ...) {
   # Find exon bases out of intron-contained coverage
   #     & Take log-transformation.
   if (is.null(logshift.val)) {
-    logshift.val = getShiftParam(X=data, plot=FALSE, ...)$optim.param
+    logshift.val = getShiftParam(X=data, plot=FALSE, param.grid=param.grid,...)$optim.param
   }
   outdata = log10(data + logshift.val) - log10(logshift.val) ;
   return(list(outdata=outdata,logshift.val=logshift.val))
