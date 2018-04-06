02800be0609149835403427c231b20f6432f8659
Hyo Young Choi <lucienne1216@gmail.com>
2018-04-05 23:22:51 -0400

    offset - add loop

--- R/Relist.hy.R
+++ R/Relist.hy.R
@@ -1,10 +1,14 @@
 Relist.hy = function(a,b){
   # Input  : a = original list; b = number will be removed from a
   # Output : c = new list; d => d[k] = case number in c of a[k]
-  c = a[-b]; d = rep(0,length(a));
-  for (i in 1:length(a)){
-    if (length(which(c==a[i]))>0){
-      d[i] = which(c==a[i]);
+  if (is.null(b)) {
+    c = a; d = a;
+  } else {
+    c = a[-b]; d = rep(0,length(a));
+    for (i in 1:length(a)){
+      if (length(which(c==a[i]))>0){
+        d[i] = which(c==a[i]);
+      }
     }
   }
   return(list(new=c,ord=d));
