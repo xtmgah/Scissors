15c92a8cca7d11dcbff53e26ec770fff94a7a077
Hyo Young Choi <lucienne1216@gmail.com>
2018-05-15 18:41:23 -0400

    NaN error fixed

--- R/detect_onoffout.R
+++ R/detect_onoffout.R
@@ -21,10 +21,11 @@ detect_onoffout = function(resmat,rawmat,exonset,
 
   onoff_stat = apply(rbind(res_on$on_stat,res_off$off_stat),2,max)
   fivenumber = fivenum(onoff_stat)
+  MOD = matrix(0,nrow=nrow(resmat),ncol=ncol(resmat))
 
   if (fivenumber[3]<=0) {
     cutoff=NULL; onoff_site = NULL;
-    nout = 0; MOD = NULL;
+    nout = 0;
   } else {
     onoff_i = apply(rbind(res_on$on_stat,res_off$off_stat),2,which.max)
     # 1 = on; 2 = off;
@@ -33,7 +34,6 @@ detect_onoffout = function(resmat,rawmat,exonset,
     onoff_site[whichon,] = res_on$on_site[whichon,]
     onoff_site[whichoff,] = res_off$off_site[whichoff,]
 
-    MOD = matrix(0,nrow=nrow(resmat),ncol=ncol(resmat))
     for (j in 1:ncol(resmat)) {
       temp_vec = rep(0,nrow(resmat))
       temp_site = onoff_site[j,]
