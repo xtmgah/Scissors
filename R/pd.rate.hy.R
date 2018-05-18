0c89de39fc67aaa6e04740595088ecff95bf920c
Hyo Young Choi <lucienne1216@gmail.com>
2018-05-18 18:28:36 -0400

    NaN error fixed

--- R/pd.rate.hy.R
+++ R/pd.rate.hy.R
@@ -1,4 +1,11 @@
+#' Projection depth
+#' @export
 pd.rate.hy = function(x) {
   # projection depth
-  return((x-median(x))/mad(x))
+  m = mad(x)
+  if (m<1e-5) {
+    return(rep(0,length(x)))
+  } else {
+    return((x-median(x))/m)
+  }
 }
