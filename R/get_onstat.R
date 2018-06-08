443e153a89f37e19d3797a811d9cf058c8442cdd
Hyo Young Choi <lucienne1216@gmail.com>
2018-06-07 22:20:30 -0400

    1 more condition to intron outliers

--- R/get_onstat.R
+++ R/get_onstat.R
@@ -18,21 +18,25 @@ get_onstat= function(resmat,rawmat,exonset,readconstr=10) {
     # read_max[j] = max(medvec[carea])
     # medianmat[j,] = apply(rawmat[carea,],2,FUN=function(x){(median(x)>=readconstr)})
     # medianmat[j,] = apply(rawmat[carea,],2,FUN=function(x){(quantile(x)[4]>=readconstr)})
-    if ((j/2)==round(j/2)) {
-      # check intron retention
-      #   1. 75 percentile > readconstr; OR
-      #   2. 75 percentile > 0.2*min(median(two adjacent exons))
-      carea1 = c(window_site[j-1]:(window_site[j-1]+window_size[j-1]-1))
-      carea2 = c(window_site[j+1]:(window_site[j+1]+window_size[j+1]-1))
-      medianmat[j,] = apply(rawmat,2,
-                            FUN=function(x){y=quantile(x[carea],probs=0.75);
-                                            readconstr2=max(5,0.1*max(quantile(x[carea1],probs=0.5),quantile(x[carea2],probs=0.5)));
-                                            ((y>=readconstr) | (y>=readconstr2))})
-    } else {
-      medianmat[j,] = apply(rawmat[carea,],2,FUN=function(x){(quantile(x,probs=0.75)>=readconstr)})
+    if (length(carea)>=10) { # 10? this should be confirmed!!!!!!!!!!!!!!!
+      if ((j/2)==round(j/2)) {
+        # check intron retention
+        #   1. 75 percentile > readconstr; OR
+        #   2. 75 percentile > 0.2*min(median(two adjacent exons))
+        #   3. 75 percentile of one of the adjacent exons > readconstr
+        carea1 = c(window_site[j-1]:(window_site[j-1]+window_size[j-1]-1))
+        carea2 = c(window_site[j+1]:(window_site[j+1]+window_size[j+1]-1))
+        medianmat[j,] = apply(rawmat,2,
+                              FUN=function(x){y=quantile(x[carea],probs=0.75);
+                              max_exonread = max(quantile(x[carea1],probs=0.5),quantile(x[carea2],probs=0.5));
+                              if (max_exonread>readconstr) {readconstr2=max(5,0.1*max_exonread)} else {readconstr2=readconstr};
+                              ((y>=readconstr) | (y>=readconstr2))})
+      } else {
+        medianmat[j,] = apply(rawmat[carea,],2,FUN=function(x){(quantile(x,probs=0.75)>=readconstr)})
+      }
+      pdrate[j,] = pd.rate.hy(apply(resmat[carea,],2,sum))
+      # cat(paste(j,"   | nan =",sum(is.nan(pdrate[j,]))),"\n")
     }
-    pdrate[j,] = pd.rate.hy(apply(resmat[carea,],2,sum))
-    cat(paste(j,"   | nan =",sum(is.nan(pdrate[j,]))),"\n")
   }
   on_stat = apply(pdrate*medianmat,2,max)
   where_on = apply(pdrate*medianmat,2,which.max)
