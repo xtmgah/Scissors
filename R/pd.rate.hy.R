5dffe9cddacdaa621a7d5b46ae712004a8a8c71c
hyochoi <lucienne1216@gmail.com>
2018-10-30 18:01:10 -0500

    asymmetriy considered

--- R/pd.rate.hy.R
+++ R/pd.rate.hy.R
@@ -1,11 +1,16 @@
 #' Projection depth
 #' @export
-pd.rate.hy = function(x) {
+pd.rate.hy = function(x,qrsc=FALSE) {
   # projection depth
   m = mad(x)
   if (m<1e-5) {
     return(rep(0,length(x)))
   } else {
-    return((x-median(x))/m)
+    if (qrsc) {
+      rsc=compScales(x)
+      return((x-median(x))/rsc$sa)
+    } else {
+      return((x-median(x))/m)
+    }
   }
 }
