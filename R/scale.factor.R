fb279ee8e4d1ff8ea4ed12a4abf027a3c613ca80
hyochoi <lucienne1216@gmail.com>
2018-10-30 19:12:35 -0500

    robust.lm considered

--- R/scale.factor.R
+++ R/scale.factor.R
@@ -1,11 +1,16 @@
 #' Obtain median scale factors based on a adjusted center
 #'
 #' @export
-scale.factor=function(X,average="mean",trim=0.1,adjval=NULL) {
+scale.factor=function(X,average="mean",trim=0.1,adjval=NULL,robust.lm=TRUE) {
 
   mean.vec = apply(X,1,FUN=function(x){adj.center(x,average=average,trim=trim,adjval=adjval)})
   if (sum(mean.vec^2)>0) {
-    msf = as.vector(t(mean.vec)%*%X/(sum(mean.vec^2)));
+    if (robust.lm) {
+      require(MASS)
+      msf = apply(X,2,FUN=function(x){rlm(x ~ mean.vec - 1,psi=psi.bisquare)$coefficients})
+    } else {
+      msf = as.vector(t(mean.vec)%*%X/(sum(mean.vec^2)));
+    }
   } else {
     msf = rep(0,ncol(X))
   }
